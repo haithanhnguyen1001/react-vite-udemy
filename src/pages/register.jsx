195b967843e1ed7056d23fca627055e213901bfd
Hai Thanh <haithanh.nguyen1001@gmail.com>
2024-09-22 23:49:16 +0700

    register user using uncontrolled component

--- src/pages/register.jsx
+++ src/pages/register.jsx
@@ -1,5 +1,108 @@
+import { Button, Form, Input, message, notification } from "antd";
+import { registerUserApi } from "../services/api.service";
+import { useNavigate } from "react-router-dom";
+
 const RegisterPage = () => {
-  return <div>Register Component</div>;
+  const [form] = Form.useForm();
+  const navigate = useNavigate();
+  const onFinish = async (values) => {
+    console.log(values);
+    const res = await registerUserApi(
+      values.fullName,
+      values.email,
+      values.password,
+      values.phone
+    );
+    if (res.data) {
+      notification.success({
+        message: "Register User",
+        description: "Dang ky thanh cong!",
+      });
+      navigate("/login");
+    } else {
+      notification.error({
+        message: "Register User Error",
+        description: JSON.stringify(res.message),
+      });
+    }
+  };
+  return (
+    <Form
+      form={form}
+      layout="vertical"
+      onFinish={onFinish}
+      // onFinishFailed={onFinishFailed}
+    >
+      <div
+        style={{
+          padding: "50px",
+        }}
+      >
+        <div
+          style={{
+            display: "flex",
+            flexDirection: "column",
+          }}
+        >
+          <Form.Item
+            label="Full Name"
+            name="fullName"
+            rules={[
+              {
+                required: true,
+                message: "Please input your fullName!",
+              },
+            ]}
+          >
+            <Input />
+          </Form.Item>
+          <Form.Item
+            label="Email"
+            name="email"
+            rules={[
+              {
+                required: true,
+                message: "Please input your email!",
+              },
+            ]}
+          >
+            <Input />
+          </Form.Item>
+          <Form.Item
+            label="Password"
+            name="password"
+            rules={[
+              {
+                required: true,
+                message: "Please input your password!",
+              },
+            ]}
+          >
+            <Input.Password />
+          </Form.Item>
+          <Form.Item
+            label="Phone number"
+            name="phone"
+            rules={[
+              {
+                required: true,
+                pattern: new RegExp(/\d+/g),
+                message: "Wrong format!",
+              },
+            ]}
+          >
+            <Input />
+          </Form.Item>
+
+          <div>
+            <Button type="primary" htmlType="submit">
+              Register
+            </Button>
+          </div>
+        </div>
+      </div>
+    </Form>
+  );
 };
 
 export default RegisterPage;
--- src/services/api.service.js
+++ src/services/api.service.js
@@ -55,6 +55,17 @@ const updateUserWithAvatarApi = (avatar, _id, fullName, phone) => {
   return axios.put(URL_BACKEND, data);
 };
 
+const registerUserApi = (fullName, email, password, phone) => {
+  const URL_BACKEND = "/api/v1/user/register";
+  const data = {
+    fullName: fullName,
+    email: email,
+    password: password,
+    phone: phone,
+  };
+  return axios.post(URL_BACKEND, data);
+};
+
 export {
   createUserApi,
   fetchAllUserAPI,
@@ -62,4 +73,5 @@ export {
   deleteUserApi,
   handleUploadFile,
   updateUserWithAvatarApi,
+  registerUserApi,
 };
